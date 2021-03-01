a5712f1e1bde8f2a56a1077972ed67d7ebf53e9d
brandonLee <lkc2252@gmail.com>
2021-03-01 21:36:10 +0900

    - dummy commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -23,21 +23,21 @@
       <div class="mobile-title mobile-visible-block-only">
         <!-- 혹시 몰라서 남겨둡니다 -->
         <!--<div class="mobile-title__wrap-icon">-->
-          <!--<img-->
-          <!--class="mobile-title__wrap-icon__icon"-->
-          <!--src="@/assets/images/icons/primary-cat.png"-->
-          <!--alt="somi"-->
-          <!--/>-->
-          <!--</div>-->
-          <!--<h2 class="title-text font-bold">-->
-          <!--소 미<br />일 기-->
-          <!--</h2>-->
+        <!--<img-->
+        <!--class="mobile-title__wrap-icon__icon"-->
+        <!--src="@/assets/images/icons/primary-cat.png"-->
+        <!--alt="somi"-->
+        <!--/>-->
+        <!--</div>-->
+        <!--<h2 class="title-text font-bold">-->
+        <!--소 미<br />일 기-->
+        <!--</h2>-->
         <!--</div>-->
         <div class="mobile-title__wrap-single-icon">
           <img
-                  class="mobile-title__wrap-single-icon__icon"
-                  src="@/assets/images/icons/somis-diary-main-icon.png"
-                  alt="somi"
+            class="mobile-title__wrap-single-icon__icon"
+            src="@/assets/images/icons/somis-diary-main-icon.png"
+            alt="somi"
           />
         </div>
       </div>
@@ -96,44 +96,44 @@ export default {
                     width: 400px;
                     height: 400px;
                 }
-              /*
-                &__text {
-                    color: $sd-blue;
-                    font-size: 100px;
-                    font-weight: 900;
-                    letter-spacing: 1.5px;
-                }
-                */
+                /*
+                  &__text {
+                      color: $sd-blue;
+                      font-size: 100px;
+                      font-weight: 900;
+                      letter-spacing: 1.5px;
+                  }
+                  */
             }
             .mobile-title {
-              /* &__wrap-icon {
-                    position: relative;
-                    width: 150px;
-                    height: 150px;
-                    background-color: $sd-blue;
-                    margin: 0 auto;
-                    border-radius: 20px;
-                    &__icon {
-                        position: absolute;
-                        left: 50%;
-                        top: 50%;
-                        width: 100px;
-                        height: 100px;
-                        transform: translate(-50%, -50%);
-                    }
-                } */
+                /* &__wrap-icon {
+                      position: relative;
+                      width: 150px;
+                      height: 150px;
+                      background-color: $sd-blue;
+                      margin: 0 auto;
+                      border-radius: 20px;
+                      &__icon {
+                          position: absolute;
+                          left: 50%;
+                          top: 50%;
+                          width: 100px;
+                          height: 100px;
+                          transform: translate(-50%, -50%);
+                      }
+                  } */
                 &__wrap-single-icon {
-                  position: relative;
-                  text-align: center;
-                  &__icon {
-                    width: 200px;
-                    height: 200px;
-                  }
+                    position: relative;
+                    text-align: center;
+                    &__icon {
+                        width: 200px;
+                        height: 200px;
+                    }
                 }
                 .title-text {
                     color: $sd-blue;
-                  text-shadow: -1px 0 $sd-deep-blue, 0 1px $sd-deep-blue, 1px 0 $sd-deep-blue, 0 -1px $sd-deep-blue;
-                  font-size: 60px;
+                    text-shadow: -1px 0 $sd-deep-blue, 0 1px $sd-deep-blue, 1px 0 $sd-deep-blue, 0 -1px $sd-deep-blue;
+                    font-size: 60px;
                     text-align: center;
                     padding: 20px 0;
                     letter-spacing: 2px;
